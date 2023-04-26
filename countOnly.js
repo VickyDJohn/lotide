c983111a18bf8330cc7195061091b2bbe314c84d
Vicky John <vicky.d.john@email.com>
2023-04-25 20:49:29 -0700

    checking for specific items and then addind to results

--- countOnly.js
+++ countOnly.js
@@ -9,10 +9,12 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+       results[item] = 1;
+      }
     }
   }
   return results;
