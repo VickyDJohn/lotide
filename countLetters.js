bfa1a44f9cd9b2daa8e53754cf8bf889ea74c5d2
Vicky John <vicky.d.john@email.com>
2023-04-26 16:59:11 -0700

    Changing for...of to regualr for loop

--- countLetters.js
+++ countLetters.js
@@ -7,10 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(str) {
-  let numLetters = [];
-  for (let letter of str) {
-    console.log(letter);
+  let numLetters = {};
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
   }
 }
 
-countLetters("lighthouse in the house")
+console.log(countLetters("lighthouse in the house"));
