0a9df6d91083530f8e8072efd81d21d07908811d
Vicky John <vicky.d.john@email.com>
2023-04-27 10:44:57 -0700

    Made changes to eqObjects function to fix errors

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -18,22 +18,42 @@ const eqObjects = function(object1, object2) {
   }
   for (let keyValue of keys1) {
     if (Array.isArray(object1[keyValue]) && Array.isArray(object2[keyValue])) {
-      return eqArrays(Object.values(object1[keyValue]), Object.values(object2[keyValue]))
-    } else if (Object.keys(object1)[keyValue] === Object.keys(object2)[keyValue]) {
-      return true;
-    } else {
+      if (eqArrays(Object.values(object1[keyValue]) !== Object.values(object2[keyValue]))) {
+        return false;
+      }
+    } else if (object1[keyValue] !== object2[keyValue]) {
       return false;
     }
   }
-}
+  return true;
+};
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`✔️  Assertion Passed: [${actual}]  ===  [${expected}]`);
+    console.log(`✔️  Assertion Passed: ${inspect(actual)}  ===  ${inspect(expected)}]`);
   } else {
-    console.log(`❌ Assertion Failed: [${actual}]  !== [${expected}]`);
+    console.log(`❌ Assertion Failed: ${inspect(actual)}  !==  ${inspect(expected)}`);
   }
 }
 
-assertObjectsEqual({a: 1, b: 2}, {b: 2, a: 1}); // expected output: Assertion Passed
-assertObjectsEqual({a: 1, b: 2}, {a: 1, b: 2, c: 3}); // expected output: Assertion Failed
+// // Test case 1 - identical objects
+// const obj1 = { a: 1, b: 2 };
+// const obj2 = { a: 1, b: 2 };
+// assertObjectsEqual(obj1, obj2); // should pass
+
+// // Test case 2 - different key order
+// const obj3 = { b: 2, a: 1 };
+// assertObjectsEqual(obj1, obj3); // should pass
+
+// // Test case 3 - different values for same keys
+// const obj4 = { a: 3, b: 2 };
+// assertObjectsEqual(obj1, obj4); // should fail
+
+// // Test case 4 - extra key in one object
+// const obj5 = { a: 1, b: 2, c: 3 };
+// assertObjectsEqual(obj1, obj5); // should fail
+
+// // Test case 5 - missing key in one object
+// const obj6 = { a: 1 };
+// assertObjectsEqual(obj1, obj6); // should fail
