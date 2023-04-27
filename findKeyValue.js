806e5d58bdda2b7e205ffee87d07a7c8da003092
Vicky John <vicky.d.john@email.com>
2023-04-26 18:44:21 -0700

    added for..in loop to loop through object

---
+++ findKeyValue.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️  Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual}  !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for(let val in object) {
+    
+  }
+}
