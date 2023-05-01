19677fda339578dc2a162d8b470316ddc42a9bd5
Vicky John <vicky.d.john@email.com>
2023-04-30 18:14:02 -0700

    Added for loop to loop through objects

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️  Assertion Passed: ${actual}  ===  ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual}  !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let result = "";
+  for (let val in object) {
+    console.log(val);
+  }
+  return result;
+};
+
+  console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  }, x => x.stars === 2));
