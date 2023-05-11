429124556ed0bd5f03c65effe5f3559db194f091
Vicky John <vicky.d.john@email.com>
2023-05-10 18:51:20 -0700

    Modified eqArraysTest to incorporate Mocha Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,13 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+const assert = require('chai').assert;
+
+describe("#eqArrays", () => {
+  it("returns true when checking if [1, 2, 3] equals [1, 2, 3]", () => {
+    const array1 = [1, 2, 3];
+    const array2 = [1, 2, 3];
+    assert.strictEqual(eqArrays(array1, array2), true);
+  });
+  it("returns false when checking if ['1', '2', '3'] equals ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+});
