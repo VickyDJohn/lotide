aad8c01833effe0c128369282b3a7f2815e87ce7
Vicky John <vicky.d.john@email.com>
2023-05-10 19:05:09 -0700

    Modified assertEqualTest to use Mocha and Chai for testing

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,2 +1,11 @@
 const assertEqual = require('../assertEqual');
-assertEqual(1, 1);
+const assert = require('chai').assert;
+
+describe("#assertEqual", () => {
+  it('should return true when values are equal', function() {
+    assert.strictEqual(assertEqual(5, 5), '✔️ Assertion Passed: 5 === 5');
+  });
+  it("returns false when checking if 1 equals a", () => {
+    assert.strictEqual(assertEqual(1, "a"), '❌ Assertion Failed: 1 !== a');
+  });
+});
