27e0a89cba811aea271d0b04847cdaa8207c9754
Vicky John <vicky.d.john@email.com>
2023-05-10 19:24:59 -0700

    Modified assertArraysEqualTest.js to use Mocha and Chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,11 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+const assert = require('chai').assert;
+
+describe("#assertArraysEqual", () => {
+  it('should return assertion passed when both arrays are equal', function() {
+    assert.strictEqual(assertArraysEqual([1,2,3], [1,2,3]), '✔️ Assertion Passed: [1,2,3] === [1,2,3]');
+  });
+  it("should return assertion failed when both arrays are unequal", () => {
+    assert.strictEqual(assertArraysEqual([1,2,3], [3,2,1]), '❌ Assertion Failed: [1,2,3] !== [3,2,1]');
+  });
+});
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,10 +2,10 @@ const assertEqual = require('../assertEqual');
 const assert = require('chai').assert;
 
 describe("#assertEqual", () => {
-  it('should return true when values are equal', function() {
+  it('should return ✔️ Assertion Passed: 5 === 5 when values are equal', function() {
     assert.strictEqual(assertEqual(5, 5), '✔️ Assertion Passed: 5 === 5');
   });
-  it("returns false when checking if 1 equals a", () => {
+  it("returns ❌ Assertion Failed: 1 !== a when checking if 1 equals a", () => {
     assert.strictEqual(assertEqual(1, "a"), '❌ Assertion Failed: 1 !== a');
   });
 });
