58a9a39de91afa67a6a2e83bbdc91d702e69bd2f
Vicky John <vicky.d.john@email.com>
2023-05-10 18:39:55 -0700

    Modified middleTest to incorporate Mocha Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,12 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+describe("#middle", () => {
+  it("returns middle element of array with even number of elements", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns middle element of array with odd number of elements", () => {
+    const array = [];
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
