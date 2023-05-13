966f3a78aceee51a9501eee5d2d62cf01cd8398e
Vicky John <vicky.d.john@email.com>
2023-05-13 13:35:24 -0700

    Refactored by using require and export

--- flatten.js
+++ flatten.js
@@ -1,22 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✔️  Assertion Passed: [${arr1}]  ===  [${arr2}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${arr1}]  !== [${arr2}]`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const flatten = function(array) {
   const flattenedArray = [];
@@ -31,3 +14,5 @@ const flatten = function(array) {
   }
   return flattenedArray;
 };
+
+module.exports = flatten;
