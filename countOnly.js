71d6b16f5e50fe9f101ca9e4b2643047838f0c1a
Vicky John <vicky.d.john@email.com>
2023-05-13 14:03:27 -0700

    refqactored countOnly to use module.exports

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️  Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual}  !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
@@ -13,28 +7,30 @@ const countOnly = function(allItems, itemsToCount) {
       if (results[item]) {
         results[item] += 1;
       } else {
-       results[item] = 1;
+        results[item] = 1;
       }
     }
   }
   return results;
-}
+};
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+module.exports = countOnly;
 
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
 
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
--- letterPositions.js
+++ letterPositions.js
@@ -1,34 +1,19 @@
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✔️  Assertion Passed: [${arr1}]  ===  [${arr2}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${arr1}]  !== [${arr2}]`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
 
 const letterPositions = function(sentence) {
   let results = {};
   for (i = 0; i < sentence.length; i++) {
     if (results[sentence[i]]) {
-      results[sentence[i]].push(i)
+      results[sentence[i]].push(i);
     } else {
       results[sentence[i]] = [i];
-      }
     }
+  }
   return results;
 };
+
+module.exports = letterPositions;
 
 // const result3 = letterPositions("goodbye");
 // assertArraysEqual(result3['g'], [0]);
